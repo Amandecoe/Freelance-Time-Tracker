f1e8bcbb8e98424c5ad5b781683b38d298712ecc
Amandecoe <amanuelbehailu557@gmail.com>
2025-11-05 19:23:23 +0300

    models for the Projects model defined

--- Backend/projects/models.py
+++ Backend/projects/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
+    id = models.DecimalField(primary_key=True)
     name = models.CharField(max_length=15)
     client_name = models.CharField(max_length=15)
     description = models.TextField()
     hourly_rate = models.DecimalField(decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
