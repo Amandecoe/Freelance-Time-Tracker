da8564bb3d72bcd80daff1318a7c8ca6c964a5ba
Amandecoe <amanuelbehailu557@gmail.com>
2025-11-05 07:02:22 +0300

    models defining started for projects

--- Backend/projects/models.py
+++ Backend/projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=15)
+    client_name = models.CharField(max_length=15)
+    description = models.TextField()
+    hourly_rate = models.DecimalField(decimal_places=2)
