89bf300c48b7e350479a4b0358de4fbbf47ea425
Amandecoe <amanuelbehailu557@gmail.com>
2025-11-06 13:31:13 +0300

    working on the views that updates a project

--- Backend/projects/views.py
+++ Backend/projects/views.py
@@ -7,18 +7,29 @@ from django.contrib.auth import get_user_model
 # Create your views here.
 
 User = get_user_model()
-@api_view(["GET"])
+
+@api_view(['GET'])
 def List_Projects(request):
     project = Project.object.filter(user = request.user)
     serialzier = ProjectSerializer(project)
     return Response(serialzier.data)
 
-@api_view('[POST]')
+@api_view(['POST'])
 def add_project(request):
    name = request.data.get("name")
    description = request.data.get("description")
    newproject = ProjectSerializer(data=request.data) #accepts all the data sent by the user to create a new project
    if newproject.is_valid(): #checks if all the input data from the user matches the one's required to create a new project
        newproject.save() #saves the new project
+       return Response(newproject.data, "Project created")
    if Project.objects.filter(name=name,description=description).exists():
        return Response("Project already exists")    
+   
+@api_view(['PUT'])
+def update_project(request,pk):
+    try:
+        project = Project.objects.get(pk = pk)
+    except: Project.DoesnotExist:
+        return Response("Project Doesn't Exist")
+    serializer = ProjectSerializer(project, data=request.data)
+    if serializer.is_valid():
