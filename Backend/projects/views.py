b3869e63ce6af2f5a017148c950780b7189e9c58
Amandecoe <amanuelbehailu557@gmail.com>
2025-11-06 12:22:53 +0300

    views modified

--- Backend/projects/views.py
+++ Backend/projects/views.py
@@ -3,10 +3,12 @@ from rest_framework.decorators import api_view
 from .models import Project
 from .serializers import ProjectSerializer
 from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 # Create your views here.
 
+User = get_user_model()
 @api_view(["GET"])
 def List_Project(request):
-    project = Project.object.filter()
-    serialzier = ProjectSerializer(Project)
+    project = Project.object.filter(user = request.user)
+    serialzier = ProjectSerializer(project)
     return Response(serialzier.data)
