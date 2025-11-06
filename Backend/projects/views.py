6ce25cdd868be1d205602e82696a271766ca48c5
Amandecoe <amanuelbehailu557@gmail.com>
2025-11-06 12:19:51 +0300

    List project view created

--- Backend/projects/views.py
+++ Backend/projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from .models import Project
+from .serializers import ProjectSerializer
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(["GET"])
+def List_Project(request):
+    project = Project.object.filter()
+    serialzier = ProjectSerializer(Project)
+    return Response(serialzier.data)
