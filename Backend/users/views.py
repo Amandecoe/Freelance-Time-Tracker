0f8da3d17ccbf0bfd64d317b3abae2711c2fd782
Amanuel <amanuelbehailu557@gmail.com>
2025-12-03 20:34:24 +0300

    create account view

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import render
+from .models import Freelancer
+from .serializers import FreelancerSerializer
 from rest_framework.decorators import api_view
 # Create your views here.
 
 @api_view(['POST'])
 def create_account(request):
+    user = Freelancer.object.filter(request.user)
+    serializer = FreelancerSerializer(user)
