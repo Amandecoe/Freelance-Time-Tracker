2a093804751234233b0692e04eacf42f89c07abc
Amanuel <amanuelbehailu557@gmail.com>
2025-12-03 19:47:12 +0300

    report app added

--- Backend/freelance_tracker/settings.py
+++ Backend/freelance_tracker/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects',
-    'sessions',
+    'timesessions',
     'users',
     'rest_framework',
 ]
--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class User(models.Model):
-    id = models.DecimalField(primarykey = True)
+#Django has a built in user model with firstname, lastname, email, password etc properties so we only need
+# a field that relates that user to a freelancer account
+class Freelancer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank = True)
+    skills = models.TextField(blank=True)
+
+
