ac283e089af3a4836ee255488d8b8d8f3428932d
Amanuel <amanuelbehailu557@gmail.com>
2025-12-03 18:33:05 +0300

    Sessions database started

--- Backend/sessions/models.py
+++ Backend/sessions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Timelog(models.Model):
+    id = models.DecimalField(primary_key=True)
+    project_id = models.DecimalField()
+    start_time = models.DateTimeField()
+    duration = models.DurationField()
