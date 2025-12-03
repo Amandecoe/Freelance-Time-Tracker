0c84c8f67ea9f35298547d26bad3e474d6a868b0
Amanuel <amanuelbehailu557@gmail.com>
2025-12-03 22:21:07 +0300

    Forms for creating a user done

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import slugify
 # Create your models here.
 #Django has a built in user model with firstname, lastname, email, password etc properties so we only need
 # a field that relates that user to a freelancer account
