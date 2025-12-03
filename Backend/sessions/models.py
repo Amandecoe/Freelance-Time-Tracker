7ee10af9493cc805d81c34960b6a9b18422d76e1
Amanuel <amanuelbehailu557@gmail.com>
2025-12-03 18:54:56 +0300

    Freelancer profile table created

--- Backend/sessions/models.py
+++ Backend/sessions/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from projects.models import Project
 # Create your models here.
 class Timelog(models.Model):
     id = models.DecimalField(primary_key=True)
     project_id = models.DecimalField()
     start_time = models.DateTimeField()
     duration = models.DurationField()
+    project = models.Foreignkey(Project, on_delete = models.CASCADE)
