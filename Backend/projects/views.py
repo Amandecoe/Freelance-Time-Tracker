fbe5e82d4f49188d6fcc727c1b1dfe6ff543c1f6
Amanuel <amanuelbehailu557@gmail.com>
2025-12-03 20:23:04 +0300

    views started for user

--- Backend/projects/views.py
+++ Backend/projects/views.py
@@ -6,8 +6,7 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 
-
-User = get_user_model()
+#no need for get_user_model because we are using django's custom user model
 
 @api_view(['GET'])
 def List_Projects(request):
@@ -24,15 +23,15 @@ def add_project(request):
        newproject.save() #saves the new project
        return Response(newproject.data, "Project created")
    if Project.objects.filter(name=name,description=description).exists():
-       return Response("Project already exists")    
-   
+       return Response("Project already exists")
+
 @api_view(['PUT'])
 def update_project(request,pk):
     try:
         project = Project.objects.get(pk = pk)
     except Project.DoesnotExist():
         return Response("Project Doesn't Exist")  #checks if the project exists and continues if it does but doesn't continue if the project doesn't exist
-    
+
     updated_project = ProjectSerializer(project, data=request.data)
     if updated_project.is_valid(): #checks if the updated project consists of all the required fields of a project
         updated_project.save()
@@ -44,7 +43,7 @@ def project_search(request):
     query = request.query_params.get("query")
     if not query:
         return Response("Nothing Found Under That Name")
-    
+
     projects = Project.objects.filter(Q(name__iscontains=query) | Q(description__iscontains=query)) #Q is a django built in feature that allows you to search
     searched_product = ProjectSerializer(projects, many = True) #can return many products that match the result
     return Response(searched_product.data)
