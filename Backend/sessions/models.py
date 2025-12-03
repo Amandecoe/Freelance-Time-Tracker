61cbb006e78f9575b48c4c2fbdc492f527af551a
Amanuel <amanuelbehailu557@gmail.com>
2025-12-03 19:22:45 +0300

    duration for a session feature created

--- Backend/sessions/models.py
+++ Backend/sessions/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from projects.models import Project
+from users.models import Freelancer
 # Create your models here.
 class Timelog(models.Model):
     id = models.DecimalField(primary_key=True)
-    project_id = models.DecimalField()
-    start_time = models.DateTimeField()
-    duration = models.DurationField()
     project = models.Foreignkey(Project, on_delete = models.CASCADE)
+    freelancer = models.ForeignKey(Freelancer, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    duration = models.DurationField(null=True, blank=True)
